80b5a44a95c1cf1e756b81e64d356d6df6b75faf
Donald Wilson <donaldww@icepointmedia.ca>
2023-04-27 08:43:33 -0400

    Add clippy enhancements

--- gcd/src/main.rs
+++ gcd/src/main.rs
@@ -1,15 +1,13 @@
-#![warn(rust_2018_idioms)]
+// #![warn(rust_2018_idioms)]
 #![allow(elided_lifetimes_in_paths)]
 
 fn gcd(mut n: u64, mut m: u64) -> u64 {
     assert!(n != 0 && m != 0);
     while m != 0 {
         if m < n {
-            let t = m;
-            m = n;
-            n = t;
+            std::mem::swap(&mut m, &mut n);
         }
-        m = m % n;
+        m %= n;
     }
     n
 }
@@ -18,23 +16,20 @@ fn gcd(mut n: u64, mut m: u64) -> u64 {
 fn test_gcd() {
     assert_eq!(gcd(14, 15), 1);
 
-    assert_eq!(gcd(2 * 3 * 5 * 11 * 17,
-                   3 * 7 * 11 * 13 * 19),
-               3 * 11);
+    assert_eq!(gcd(2 * 3 * 5 * 11 * 17, 3 * 7 * 11 * 13 * 19), 3 * 11);
 }
 
-use std::str::FromStr;
 use std::env;
+use std::str::FromStr;
 
 fn main() {
     let mut numbers = Vec::new();
 
     for arg in env::args().skip(1) {
-        numbers.push(u64::from_str(&arg)
-                     .expect("error parsing argument"));
+        numbers.push(u64::from_str(&arg).expect("error parsing argument"));
     }
 
-    if numbers.len() == 0 {
+    if numbers.is_empty() {
         eprintln!("Usage: gcd NUMBER ...");
         std::process::exit(1);
     }
@@ -44,6 +39,5 @@ fn main() {
         d = gcd(d, *m);
     }
 
-    println!("The greatest common divisor of {:?} is {}",
-             numbers, d);
+    println!("The greatest common divisor of {:?} is {}", numbers, d);
 }
